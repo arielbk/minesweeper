abad4f80f98e27f1666730d8141c3436ad2d7ddf
arielbk <buchwald.kearney@gmail.com>
2021-10-21 22:08:02 +0200

    feat: vertically center game for mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,27 @@ import Grid from './components/common/Grid';
 import Controls from './components/Controls';
 import { GameProvider } from './contexts/GameContext';
 
-export const responsiveWidth = "min(90vw, 73vh)";
+export const responsiveWidth = 'min(90vw, 73vh)';
 
 const App: React.FC = () => {
   return (
     <GridProvider>
       <GameProvider>
         <Header />
-        <Flex mx="auto" mt="90px" width={responsiveWidth} userSelect="none">
+        <Flex
+          mx="auto"
+          mt="90px"
+          width={responsiveWidth}
+          userSelect="none"
+          height="calc(100vh - 90px)"
+          // vertically center the game content for mobile view
+          sx={{
+            '@media(max-width: 600px)': {
+              alignItems: 'center',
+              marginTop: 45,
+            },
+          }}
+        >
           <Box>
             <Grid />
             <Controls />
