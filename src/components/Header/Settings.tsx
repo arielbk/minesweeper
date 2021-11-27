60aab2d4f075aec47f2ffe10a65006f74e049adb
arielbk <buchwald.kearney@gmail.com>
2021-11-27 19:48:15 +0100

    feat: only allow three size options

--- src/components/Header/Settings.tsx
+++ src/components/Header/Settings.tsx
@@ -37,8 +37,6 @@ const Settings: React.FC = () => {
           <option value={5}>5</option>
           <option value={10}>10</option>
           <option value={15}>15</option>
-          <option value={20}>20</option>
-          <option value={25}>25</option>
         </select>
       </Flex>
       <IconButton
